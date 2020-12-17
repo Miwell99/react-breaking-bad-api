2e410b34e77f2133afd43f539b64ee8448fb32c5
Miwell <miguelinformatica01@gmail.com>
2020-12-17 11:36:41 +0100

    boostrap added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,42 @@ export const App = () => {
 
   return (
     <div className="App">
-        <nav>
-          <ul>
-            <li>App Name</li>
-            <li>About the App</li>
-            <li>About Me</li>
+      <nav className="navbar bg-light">
+        <a className="navbar-brand" href="#">Company</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Link</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown
+        </a>
+              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link disabled" href="#">Disabled</a>
+            </li>
           </ul>
-        </nav>
-        <div>
+          </div>
+
+      </nav>
+        <div className="container">
+          <div className="card">
+            <h1>Holaa ya tengo boostrap</h1>
+          </div>
+
           <Footer></Footer>
         </div>
     </div>
